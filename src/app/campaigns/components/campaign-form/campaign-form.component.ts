f5220d350a0caacec23e13fbbd7056a9f8bbbc08
Avital Parasha <taliofekk@gmail.com>
2020-06-23 22:00:16 +0300

    how can I get unique id for campaign?

--- src/app/campaigns/components/campaign-form/campaign-form.component.ts
+++ src/app/campaigns/components/campaign-form/campaign-form.component.ts
@@ -18,27 +18,22 @@ export class CampaignFormComponent implements OnInit {
   ngOnInit() {
   }
 
-  onClick()
+  SubmitCampaignForm()
 {
-//   var name = (document.getElementById("campaignName") as HTMLInputElement).value;
-//   var npo = (document.getElementById("campaignNpo") as HTMLInputElement).value;
-//   var cText = (document.getElementById("cText") as HTMLInputElement).value;
-//   var city = (document.getElementById("city") as HTMLInputElement).value;
-//   var startDate = (document.getElementById("startDate") as HTMLInputElement).value;
-//   var endDate = (document.getElementById("endDate") as HTMLInputElement).value;
-//  // var campId = this.authService.getLoggedUserId();
-//   console.log("name" + name + " " + npo +"  " + cText);
-//   setTimeout(function(){
-//   }, 199900000);
-//   this.db.collection('/Campaigns').doc(this.campId).set({
-//     campaignName : name,
-//     campaignNpo : npo,
-//     startDate : startDate,
-//     endDate : endDate,
-//     city : city,
-//     campaignID : this.campId,
-//     cText : cText
-//   }).then(res => {}, err => err);
-}
+  var name = (document.getElementById("campaignName") as HTMLInputElement).value;
+  var npo = (document.getElementById("campaignNpo") as HTMLInputElement).value;
+  var cText = (document.getElementById("cText") as HTMLInputElement).value;
+  var city = (document.getElementById("city") as HTMLInputElement).value;
+  var startDate = (document.getElementById("startDate") as HTMLInputElement).value;
+  var endDate = (document.getElementById("endDate") as HTMLInputElement).value;
+  this.db.collection('/Campaigns').doc().set({
+    campaignName : name,
+      NpoName : npo,
+      decription : cText,
+      city : city,
+      startDate : startDate,
+      endDate : endDate
+    }).then(res => {}, err => err);
+  }
 
 }
